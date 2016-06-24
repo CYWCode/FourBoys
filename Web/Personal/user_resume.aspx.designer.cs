8205d7aeb3193b649857c13b40ab2cfd95e8686d
chenyuwontcode <18813090681@163.com>
2016-06-24 22:42:19 +0800

    保存一下

--- Web/Personal/user_resume.aspx.designer.cs
+++ Web/Personal/user_resume.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace Web.Personal {
         protected global::System.Web.UI.WebControls.TextBox tboxAge;
         
         /// <summary>
+        /// warnAge 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label warnAge;
+        
+        /// <summary>
         /// rbtnListSex 控件。
         /// </summary>
         /// <remarks>
@@ -65,6 +74,15 @@ namespace Web.Personal {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox tboxPhone;
+        
+        /// <summary>
+        /// warnPhone 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label warnPhone;
         
         /// <summary>
         /// cboxListDirection 控件。
