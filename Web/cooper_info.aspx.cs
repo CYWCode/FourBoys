a41e0758afdd283408efe4e9ef14f45ef1af6c71
YihangFan <279411167@qq.com>
2016-06-30 20:28:57 +0800

    finish

---
+++ Web/cooper_info.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web.Personal
+{
+    public partial class cooper_info : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request["cooperId"]);
+
+            IBLL.IEnterpriseAdminData data = new BLLFac.AdminHelper().getEnterPrise(id);
+
+            lTitle.Text = data.title;
+            lName.Text = data.title;
+            lEmail.Text = data.email;
+            lScale.Text = data.scale;
+        }
+    }
+}
